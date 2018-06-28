ca760cefac66c053870cfe5cd5328b5143c6e3ba
Rommel Cuneta <cunetarommel0620@gmail.com>
2018-06-28 18:51:55 +0800

    homepage

--- src/app/front/home/home.component.ts
+++ src/app/front/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,115 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() 
+  {
+  		this.parallax();
+  }
+
+  parallax() : void
+  {
+      if("ontouchstart" in window)
+      {
+          document.documentElement.className = document.documentElement.className + " touch";
+      }
+      if(!$("html").hasClass("touch"))
+      {
+          /* background fix */
+          $(".parallax").css("background-attachment", "fixed");
+      }
+      
+      $(window).resize(this.fullscreenFix);
+      this.fullscreenFix();
+
+      $(window).resize(this.backgroundResize);
+      $(window).focus(this.backgroundResize);
+      this.backgroundResize();
+
+      if(!$("html").hasClass("touch"))
+      {
+          $(window).resize(this.parallaxPosition);
+          //$(window).focus(this.parallaxPosition);
+          $(window).scroll(this.parallaxPosition);
+          this.parallaxPosition();
+      }
+  }
+
+  fullscreenFix() : void
+  {
+      var h = $('body').height();
+      // set .fullscreen height
+      $(".content-b").each(function(i){
+              if($(this).innerHeight() > h){ $(this).closest(".fullscreen").addClass("overflow");
+          }
+      });
+  }
+
+  backgroundResize() : void
+  {
+      var windowH = $(window).height();
+      $(".background").each(function(i){
+          var path = $(this);
+          // variables
+          var contW = path.width();
+          var contH = path.height();
+          var imgW = parseInt(path.attr("data-img-width"));
+          var imgH = parseInt(path.attr("data-img-height"));
+          var ratio = imgW / imgH;
+          // overflowing difference
+          var diff = parseFloat(path.attr("data-diff"));
+          diff = diff ? diff : 0;
+          // remaining height to have fullscreen image only on parallax
+          var remainingH = 0;
+          if(path.hasClass("parallax") && !$("html").hasClass("touch")){
+              var maxH = contH > windowH ? contH : windowH;
+              remainingH = windowH - contH;
+          }
+          // set img values depending on cont
+          imgH = contH + remainingH + diff;
+          imgW = imgH * ratio;
+          // fix when too large
+          if(contW > imgW){
+              imgW = contW;
+              imgH = imgW / ratio;
+          }
+          //
+          path.data("resized-imgW", imgW);
+          path.data("resized-imgH", imgH);
+          path.css("background-size", imgW + "px " + imgH + "px");
+      });
+  }
+
+  parallaxPosition(): void
+  {
+      var heightWindow = $(window).height();
+      var topWindow = $(window).scrollTop();
+      var bottomWindow = topWindow + heightWindow;
+      var currentWindow = (topWindow + bottomWindow) / 2;
+      $(".parallax").each(function(i){
+          var path = $(this);
+          var height = path.height();
+          var top = path.offset().top;
+          var bottom = top + height;
+          // only when in range
+          if(bottomWindow > top && topWindow < bottom){
+              var imgW = path.data("resized-imgW");
+              var imgH = path.data("resized-imgH");
+              // min when image touch top of window
+              var min = 0;
+              // max when image touch bottom of window
+              var max = - imgH + heightWindow;
+              // overflow changes parallax
+              var overflowH = height < heightWindow ? imgH - height : imgH - heightWindow; // fix height on overflow
+              top = top - overflowH;
+              bottom = bottom + overflowH;
+              // value with linear interpolation
+              var value = min + (max - min) * (currentWindow - top) / (bottom - top);
+              // set background-position
+              var orizontalPosition = path.attr("data-oriz-pos");
+              orizontalPosition = orizontalPosition ? orizontalPosition : "50%";
+              $(this).css("background-position", orizontalPosition + " " + value + "px");
+          }
+      });
   }
 
 }
--- src/app/front/layout/layout.component.ts
+++ src/app/front/layout/layout.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+selector: 'app-layout',
+templateUrl: './layout.component.html',
+styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+constructor() { }
 
-  ngOnInit() {
-  }
+ngOnInit() 
+{
+	this.header_fixed();
+}
 
+	header_fixed(): void
+	{
+		// When the user scrolls the page, execute myFunction 
+		window.onscroll = function() {myFunction()};
+
+		// Get the header
+		var header = document.getElementById("pageHeader");
+
+		// Get the offset position of the navbar
+		var sticky = header.offsetTop;
+
+		// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+		function myFunction() 
+		{
+			if (window.pageYOffset > sticky) 
+			{
+				header.classList.add("sticky");
+			} 
+
+			else 
+			{
+				header.classList.remove("sticky");
+			}
+		}
+	}
 }
