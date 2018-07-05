67bc120374c9fa937a470ad9fe8191566694d391
Rommel Cuneta <cunetarommel0620@gmail.com>
2018-07-05 13:50:02 +0800

    grid

--- src/app/front/home/home.component.ts
+++ src/app/front/home/home.component.ts
@@ -3,63 +3,75 @@ import * as $ from "jquery";
 import { WOW } from 'wowjs';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit 
+{
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() 
-  {
-  		this.parallax();
-      new WOW().init();
-  }
+	ngOnInit() 
+	{
+		// this.wowjs();
+		new WOW().init();
+		this.parallax();
+	}
 
-  parallax() : void
-  {
-      if("ontouchstart" in window)
-      {
-          document.documentElement.className = document.documentElement.className + " touch";
-      }
-      if(!$("html").hasClass("touch"))
-      {
-          /* background fix */
-          $(".parallax").css("background-attachment", "fixed");
-      }
-      
-      $(window).resize(this.fullscreenFix);
-      this.fullscreenFix();
+	// wowjs() : void
+	// {
+	// 	var wow = new WOW({
+	// 	    live: false
+	// 	});
 
-      $(window).resize(this.backgroundResize);
-      $(window).focus(this.backgroundResize);
-      this.backgroundResize();
+	// 	wow.init();
+	// }
 
-      if(!$("html").hasClass("touch"))
-      {
-          $(window).resize(this.parallaxPosition);
+	parallax() : void
+	{
+		if("ontouchstart" in window)
+		{
+			document.documentElement.className = document.documentElement.className + " touch";
+		}
+
+		if(!$("html").hasClass("touch"))
+		{
+			/* background fix */
+			$(".parallax").css("background-attachment", "fixed");
+		}
+
+		$(window).resize(this.fullscreenFix);
+		this.fullscreenFix();
+
+		$(window).resize(this.backgroundResize);
+		$(window).focus(this.backgroundResize);
+		this.backgroundResize();
+
+		if(!$("html").hasClass("touch"))
+		{
+			$(window).resize(this.parallaxPosition);
           //$(window).focus(this.parallaxPosition);
           $(window).scroll(this.parallaxPosition);
           this.parallaxPosition();
-      }
+      	}
   }
 
   fullscreenFix() : void
   {
-      var h = $('body').height();
+  	var h = $('body').height();
       // set .fullscreen height
       $(".content-b").each(function(i){
-              if($(this).innerHeight() > h){ $(this).closest(".fullscreen").addClass("overflow");
-          }
-      });
+      	if($(this).innerHeight() > h){ $(this).closest(".fullscreen").addClass("overflow");
+      }
+  });
   }
 
   backgroundResize() : void
   {
-      var windowH = $(window).height();
-      $(".background").each(function(i){
-          var path = $(this);
+  	var windowH = $(window).height();
+  	$(".background").each(function(i){
+  		var path = $(this);
           // variables
           var contW = path.width();
           var contH = path.height();
@@ -72,16 +84,16 @@ export class HomeComponent implements OnInit {
           // remaining height to have fullscreen image only on parallax
           var remainingH = 0;
           if(path.hasClass("parallax") && !$("html").hasClass("touch")){
-              var maxH = contH > windowH ? contH : windowH;
-              remainingH = windowH - contH;
+          	var maxH = contH > windowH ? contH : windowH;
+          	remainingH = windowH - contH;
           }
           // set img values depending on cont
           imgH = contH + remainingH + diff;
           imgW = imgH * ratio;
           // fix when too large
           if(contW > imgW){
-              imgW = contW;
-              imgH = imgW / ratio;
+          	imgW = contW;
+          	imgH = imgW / ratio;
           }
           //
           path.data("resized-imgW", imgW);
@@ -92,19 +104,19 @@ export class HomeComponent implements OnInit {
 
   parallaxPosition(): void
   {
-      var heightWindow = $(window).height();
-      var topWindow = $(window).scrollTop();
-      var bottomWindow = topWindow + heightWindow;
-      var currentWindow = (topWindow + bottomWindow) / 2;
-      $(".parallax").each(function(i){
-          var path = $(this);
-          var height = path.height();
-          var top = path.offset().top;
-          var bottom = top + height;
+  	var heightWindow = $(window).height();
+  	var topWindow = $(window).scrollTop();
+  	var bottomWindow = topWindow + heightWindow;
+  	var currentWindow = (topWindow + bottomWindow) / 2;
+  	$(".parallax").each(function(i){
+  		var path = $(this);
+  		var height = path.height();
+  		var top = path.offset().top;
+  		var bottom = top + height;
           // only when in range
           if(bottomWindow > top && topWindow < bottom){
-              var imgW = path.data("resized-imgW");
-              var imgH = path.data("resized-imgH");
+          	var imgW = path.data("resized-imgW");
+          	var imgH = path.data("resized-imgH");
               // min when image touch top of window
               var min = 0;
               // max when image touch bottom of window
@@ -122,5 +134,7 @@ export class HomeComponent implements OnInit {
           }
       });
   }
+
+
 
 }
